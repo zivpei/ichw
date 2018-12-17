a23d05e88e71b93424a6ef394b52c9c0ffc0b225
Liu Xingpei <43376781+zivpei@users.noreply.github.com>
2018-12-17 21:41:08 +0800

    Update tile.py

--- pyassign3/tile.py
+++ pyassign3/tile.py
@@ -1,4 +1,3 @@
-import copy
 import time
 import turtle
 
@@ -20,7 +19,7 @@ def valid(i, k):
     else:
         list2 = list(range(x - w + 2, x)) + [0]
     if k <= S:
-        # 竖铺检测
+        # 竖铺检测, 越界的检测依靠同余思想
         if k - i == x * (z - 1) + w - 1 and i % x not in list2:
             return True, [j for t in range(w - 1, -1, -1)
                           for j in range(i + w - 1 - t, k - t + 1, x)], 1
@@ -49,25 +48,28 @@ def remove(a, l):
     return b
 
 
-def tile(a, b, c, total_s=0, cnt=0):
+def tile(a, b, total_s=0, cnt=0):
     """递归法实现铺砖过程，并将每一种可行的结果添加到一个列表中"""
-    global n, ans, s, S, x, y, z, w, pieces
+    global n, ans, s, S, x, y, z, w
+    # 如果已铺面积与墙的面积相等，则将结果添加到答案列表ans中
     if total_s == S:
         ans.append(b[:])
         n = n + 1
     else:
+        # 每次取列表中的第一个元素进行横竖铺砖检测
         j = a[0]
         for k in range(j + x * (w - 1) + z - 1, j + x * (z - 1) + w):
             if valid(j, k)[0]:
                 d = valid(j, k)[1]
                 if test_if_in(a, d):
                     b[cnt] = tuple(d)
-                    tile(remove(a, d), b, c, total_s + s, cnt + 1)
+                    # 将结果进行传递，思想上与八皇后问题的第一种解法类似
+                    tile(remove(a, d), b, total_s + s, cnt + 1)
 
 
 def main():
     """主程序块"""
-    global x, y, z, w, S, s, n, pieces
+    global x, y, z, w, S, s, n, pieces, ans
 
     x = int(input("Please input the length of the wall:"))
     y = int(input("Please input the width of the wall:"))
@@ -78,91 +80,99 @@ def main():
     n = 0
     pieces = int(S / s)
 
+    # 先对可铺性进行检验
     if S % s == 0:
         if z < w:
             z, w = w, z
 
+        # 列表b用来储存每一次可行的铺法
         b = [0] * pieces
-        c = [[0] for i in range(pieces)]
+        # 列表a储存着(x*y)块砖
         a = [i for i in range(1, S + 1)]
 
         t1 = time.process_time()
-        tile(a, b, c)
+        tile(a, b)
         t2 = time.process_time()
         print(ans)
         print("Total plans: ", n)
-        print("Time:", "%.8f" % (t2 - t1), "s")
+        # 打印运算时间并保留8位小数
+        print("Time:", "%.8f" % (t2 - t1) + "s")
     else:
         print("Error")
 
-    scr = turtle.Screen()
-    scr.bgcolor("black")
-    scr.colormode(255)
-    t = turtle.Turtle()
-    wr = turtle.Turtle()
-    prompt = "Input number of 0–" + str(n - 1)
-    st = int(turtle.numinput("Select plan", prompt, 0, 0, n - 1))
-    l = ans[st]
-    length = 35
-    scr.setworldcoordinates(-length * x, -length *
-                            (y + x), length * 2 * x, length * x)
+    if ans:
+        scr = turtle.Screen()
+        scr.bgcolor("black")
+        scr.colormode(255)
+        t = turtle.Turtle()
+        wr = turtle.Turtle()
+        prompt = "Input number of 0–" + str(n - 1)
+        st = int(turtle.numinput("Select plan", prompt, 0, 0, n - 1))
+        l = ans[st]
+        length = 35
+        # 依据用户输入的x、y值修改画布的边界，使得最终呈现的图形居中
+        scr.setworldcoordinates(-length * x, -length *
+                                (y + x), length * 2 * x, length * x)
 
-    t.penup()
-    wr.penup()
-    t.speed(0)
-    t.pensize(0)
-    t.pencolor((119, 66, 141))
-    wr.pencolor("white")
-    t.setheading(0)
-    wr.hideturtle()
-    # 画网格线并给砖标号
-    for i in range(y):
-        t.goto(0, -(i + 1) * length)
-        t.pendown()
-        for j in range(1, x + 1):
-            t.forward(length)
-            wr.goto(t.xcor() - 0.5 * length, t.ycor() + 0.5 * length)
-            wr.write(str(i * x + j), align="center",
-                     font=("Arial", 64 // max([x, y]), "normal"))
         t.penup()
-    t.setheading(270)
-    for i in range(x - 1):
-        t.goto((i + 1) * length, 0)
-        t.pendown()
-        t.forward(y * length)
-        t.penup()
-
-    t.pensize(3)
-    t.pencolor((255, 177, 27))
-    # 给砖块勾边
-    for i in l:
-        if int(i[0] % x) == 0:
-            t.goto(int(x - 1) * length, -int(i[0] // x - 1) * length)
-        else:
-            t.goto(int((i[0] % x) - 1) * length, -int(i[0] // x) * length)
-        t.pendown()
+        wr.penup()
+        t.speed(0)
+        t.pensize(0)
+        t.pencolor((119, 66, 141))
+        wr.pencolor("white")
+        t.setheading(0)
+        wr.hideturtle()
+        # 首先画网格线并给砖标号
+        for i in range(y):
+            t.goto(0, -(i + 1) * length)
+            t.pendown()
+            for j in range(1, x + 1):
+                t.forward(length)
+                wr.goto(t.xcor() - 0.5 * length, t.ycor() + 0.5 * length)
+                wr.write(str(i * x + j), align="center",
+                         font=("Arial", 64 // max([x, y]), "normal"))
+            t.penup()
         t.setheading(270)
-        if valid(i[0], i[-1])[2] == 1:
-            for j in range(4):
-                if j % 2 == 0:
-                    t.forward(z * length)
-                    t.left(90)
-                elif j % 2 == 1:
-                    t.forward(w * length)
-                    t.left(90)
-            t.penup()
-        else:
-            for j in range(4):
-                if j % 2 == 0:
-                    t.forward(w * length)
-                    t.left(90)
-                elif j % 2 == 1:
-                    t.forward(z * length)
-                    t.left(90)
+        for i in range(x - 1):
+            t.goto((i + 1) * length, 0)
+            t.pendown()
+            t.forward(y * length)
             t.penup()
 
-    t.hideturtle()
-    scr.exitonclick()
+        t.pensize(3)
+        t.pencolor((255, 177, 27))
+        # 给砖块勾边
+        for i in l:
+            # 将画笔置于砖块的左上角
+            if int(i[0] % x) == 0:
+                t.goto(int(x - 1) * length, -int(i[0] // x - 1) * length)
+            else:
+                t.goto(int((i[0] % x) - 1) * length, -int(i[0] // x) * length)
+            t.pendown()
+            t.setheading(270)
+            # 给竖砖勾边
+            if valid(i[0], i[-1])[2] == 1:
+                for j in range(4):
+                    if j % 2 == 0:
+                        t.forward(z * length)
+                        t.left(90)
+                    elif j % 2 == 1:
+                        t.forward(w * length)
+                        t.left(90)
+                t.penup()
+            # 给横砖勾边
+            else:
+                for j in range(4):
+                    if j % 2 == 0:
+                        t.forward(w * length)
+                        t.left(90)
+                    elif j % 2 == 1:
+                        t.forward(z * length)
+                        t.left(90)
+                t.penup()
+
+        t.hideturtle()
+        scr.exitonclick()
 
 
 if __name__ == "__main__":
